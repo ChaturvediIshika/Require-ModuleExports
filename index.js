5557f8798bb0df9f87b42bfdbd7380db6fcc1b11
ChaturvediIshika <80108033+ChaturvediIshika@users.noreply.github.com>
2023-03-28 14:38:59 +0530

    commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// const math=require('./math');
+
+
+// console.log(math);
+// console.log(math.sq(5));
+// console.log(math.add(2,3));
+
+// const {PI,sq,add}=require('./math')
+// console.log(sq(5));
+// console.log(add(2,3));
+
+const math=require('./math');
+console.log(math);
---
+++ math.js
@@ -0,0 +1,11 @@
+const PI=3.14;
+
+const sq= num=>num*num;
+
+const add=(a,b)=>a+b;
+
+// module.exports={
+//     PI,sq,add
+// }
+
+module.exports="Hello";
